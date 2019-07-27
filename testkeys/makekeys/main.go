7036b32ef63d800b8faeedbd24955ee8e39fac13
drdr xp <drdr.xp@gmail.com>
2019-07-28 02:40:26 +0800

    refactor keys maker

--- testkeys/makekeys/main.go
+++ testkeys/makekeys/main.go
@@ -4,39 +4,44 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
-	"strings"
 )
 
 func main() {
-	seg := strings.Repeat("a", 8*1024)
-	ss := []string{
-		seg + "x" + seg + "a",
-		seg + "x" + seg + "b",
-		seg + "x" + seg + "bc",
+	// seg := strings.Repeat("a", 8*1024)
+	// ss := []string{
+	//     seg + "x" + seg + "a",
+	//     seg + "x" + seg + "b",
+	//     seg + "x" + seg + "bc",
 
-		seg + "y" + seg + "a",
-		seg + "y" + seg + "ab",
-		seg + "y" + seg + "d",
+	//     seg + "y" + seg + "a",
+	//     seg + "y" + seg + "ab",
+	//     seg + "y" + seg + "d",
 
-		seg + "z" + seg + "u",
-		seg + "z" + seg + "v",
-		seg + "z" + seg + "w",
-		seg + "z" + seg + "x",
-	}
+	//     seg + "z" + seg + "u",
+	//     seg + "z" + seg + "v",
+	//     seg + "z" + seg + "w",
+	//     seg + "z" + seg + "x",
+	// }
 
-	// ss := RandVarStrings(5, 65536, nil)
+	ss := RandStrings(1000*1000, 10, 20, nil)
 
 	tmpl := `package testkeys
 var (
-	testKeys5vl64k = %#v
- )`
-	fmt.Printf(tmpl, ss)
+	testKeys5vl64k = []string{
+`
+
+	fmt.Println(tmpl)
+	for _, s := range ss {
+		fmt.Printf(`		"%s",`, s)
+		fmt.Println()
+	}
+	fmt.Printf("}\n)")
 }
 
 var alphas = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 var runes = []rune("~!@#$%^&*()_+`-=[]{};:<>?,./abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
-func RandVarStrings(n, maxLen int, from []rune) []string {
+func RandStrings(n, minLen, maxLen int, from []rune) []string {
 
 	if from == nil {
 		from = alphas
@@ -47,7 +52,7 @@ func RandVarStrings(n, maxLen int, from []rune) []string {
 	mp := make(map[string]bool, 0)
 
 	for i := 0; i < n; i++ {
-		l := rand.Intn(maxLen + 1)
+		l := rand.Intn(maxLen-minLen+1) + minLen
 		b := make([]rune, l)
 		for j := 0; j < l; j++ {
 			k := rand.Intn(rlen)
