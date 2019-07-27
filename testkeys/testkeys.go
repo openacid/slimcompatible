a2390199731e4fb90fd7860bb471675f819ad887
drdr xp <drdr.xp@gmail.com>
2019-07-28 02:38:23 +0800

    add a-million keys and 300 keys

--- testkeys/testkeys.go
+++ testkeys/testkeys.go
@@ -2,12 +2,14 @@ package testkeys
 
 var (
 	Keys = map[string][]string{
-		"10ll16k": testKeys10ll16k,
-		"10vl5":   testKeys10vl5,
-		"11vl5":   testKeys11vl5,
-		"50kl10":  testKeys50kl10,
-		"50kvl10": testKeys50kvl10,
-		"20kl10":  testKeys20kl10,
-		"20kvl10": testKeys20kvl10,
+		"10ll16k":  testKeys10ll16k,
+		"10vl5":    testKeys10vl5,
+		"11vl5":    testKeys11vl5,
+		"300vl50":  testKeys300vl50,
+		"20kl10":   testKeys20kl10,
+		"20kvl10":  testKeys20kvl10,
+		"50kl10":   testKeys50kl10,
+		"50kvl10":  testKeys50kvl10,
+		"1mvl5_10": testKeys1mvl5_10,
 	}
 )
